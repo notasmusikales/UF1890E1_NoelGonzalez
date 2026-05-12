aaacbed1cd5415306c9bb11a21ef7c82478ae471
noel <notasmusikales@gmail.com>
2026-05-12 12:03:51 +0100

    Add IF NOT EXISTS clause to table creation statements in crear-tablas.sql

--- tablas-sql/crear-tablas.sql
+++ tablas-sql/crear-tablas.sql
@@ -1,16 +1,16 @@
-CREATE TABLE dim_cliente (
+CREATE TABLE IF NOT EXISTS dw.dim_cliente (
 	id_cliente SERIAL PRIMARY KEY,
 	nombre TEXT,
 	ciudad TEXT
 );
 
-CREATE TABLE dim_producto (
+CREATE TABLE IF NOT EXISTS dw.dim_producto (
 	id_producto SERIAL PRIMARY KEY,
 	nombre TEXT,
 	categoria TEXT
 );
 
-CREATE TABLE fact_ventas (
+CREATE TABLE IF NOT EXISTS dw.fact_ventas (
 	id_venta SERIAL PRIMARY KEY,
 	id_cliente INT,
 	id_producto INT,
